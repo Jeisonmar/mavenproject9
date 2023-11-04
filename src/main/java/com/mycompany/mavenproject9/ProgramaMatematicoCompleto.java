b9316fff3c0855519703030b9d8d4770e4b7406f
Jeison <jpenafiel7591@utm.edu.ec>
2023-11-03 22:07:56 -0500

    Commit 2: Añadir clase interna GestorInteraccion

--- src/main/java/com/mycompany/mavenproject9/ProgramaMatematicoCompleto.java
+++ src/main/java/com/mycompany/mavenproject9/ProgramaMatematicoCompleto.java
@@ -1,10 +1,22 @@
+import java.util.Random;
+
 public class ProgramaMatematicoCompleto {
+    
+    public static class GestorInteraccion {
+        Random random;
+
+        public GestorInteraccion() {
+            random = new Random();
+        }
+
+        public void mostrarMensaje(String mensaje) {
+            System.out.println(mensaje);
+        }
+
+       
+    }
+
     public static void main(String[] args) {
      
-        
-        
-        
-        
-        
     }
 }
