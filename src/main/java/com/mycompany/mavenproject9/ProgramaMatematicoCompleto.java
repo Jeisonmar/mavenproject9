bc3d836469f349cca7d6d52115d8fea25b9a2d65
Jeison <jpenafiel7591@utm.edu.ec>
2023-11-03 22:10:31 -0500

    Commit 3: Implementar generación de mensajes positivos y negativos

--- src/main/java/com/mycompany/mavenproject9/ProgramaMatematicoCompleto.java
+++ src/main/java/com/mycompany/mavenproject9/ProgramaMatematicoCompleto.java
@@ -8,7 +8,30 @@ public class ProgramaMatematicoCompleto {
         public GestorInteraccion() {
             random = new Random();
         }
+public void mostrarMensajePositivo() {
+        String[] mensajesPositivos = {
+            "¡Muy bien!",
+            "¡Excelente!",
+            "¡Buen trabajo!",
+            "¡Sigue así!"
+        };
 
+        int indiceAleatorio = random.nextInt(mensajesPositivos.length);
+        System.out.println(mensajesPositivos[indiceAleatorio]);
+    }
+
+    public void mostrarMensajeNegativo() {
+        String[] mensajesNegativos = {
+            "Incorrecto. Intenta una vez más.",
+            "No es correcto, inténtalo de nuevo.",
+            "Lamentablemente, esa no es la respuesta correcta.",
+            "Sigue intentando."
+        };
+
+        int indiceAleatorio = random.nextInt(mensajesNegativos.length);
+        System.out.println(mensajesNegativos[indiceAleatorio]);
+    }
+}
         public void mostrarMensaje(String mensaje) {
             System.out.println(mensaje);
         }
@@ -19,4 +42,5 @@ public class ProgramaMatematicoCompleto {
     public static void main(String[] args) {
      
     }
+    
 }
